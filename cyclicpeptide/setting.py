3abcb0e67dbf3e7456c20da576c1ca4ca495913c
Willow0316 <13676735203@163.com>
2024-06-14 11:16:10 +0800

    update docs

--- cyclicpeptide/setting.py
+++ cyclicpeptide/setting.py
@@ -1,7 +1,7 @@
 import os
 global monomer_path, aas_path, AminoAcids_path, aa_smiles_path
 
-path_of_this_file = os.path.abspath(__file__).replace('/setting.py', '')
+path_of_this_file = os.path.abspath(__file__).replace('/cyclicpeptide/setting.py', '')
 
 monomer_path = os.path.join(path_of_this_file,'states/monomer.tsv')
 aas_path = os.path.join(path_of_this_file,'states/aas.txt')
